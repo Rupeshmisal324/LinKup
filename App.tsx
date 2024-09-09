33b6456ab2ff95d90ebf7e30fcd90696484cb4ab
Rupeshmisal324 <131134692+Rupeshmisal324@users.noreply.github.com>
2024-09-09 07:10:47 +0530

    firstcommit

--- App.tsx
+++ App.tsx
@@ -5,114 +5,36 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useEffect } from 'react';
+import type { PropsWithChildren } from 'react';
+import { Image, SafeAreaView, ScrollView, Settings, StyleSheet, Text, View, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import HomeStack from './AppScreens/MainScreens/HomeStack';
+import AuthStack from './AppScreens/AuthScreens/AuthStack';
+import { PaperProvider } from 'react-native-paper';
+import LogoScreen from './AppScreens/LogoScreen';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import HomeStack from './AppScreens/MainScreens/HomeStack';
+const MainStack = createNativeStackNavigator();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <PaperProvider>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#191C26', justifyContent: 'center' }}>
+      <NavigationContainer>
+        <MainStack.Navigator initialRouteName="LogoScreen">
+          <MainStack.Screen name="LogoScreen" component={LogoScreen} options={{ headerShown: false }} />
+          <MainStack.Screen name="AuthStack" component={AuthStack} options={{ headerShown: false }} />
+          <MainStack.Screen name="HomeStack" component={HomeStack} options={{ headerShown: false }} />
+        </MainStack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
+    </PaperProvider>
+    </GestureHandlerRootView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
--- android/app/src/main/java/com/reelive/MainActivity.kt
+++ android/app/src/main/java/com/reelive/MainActivity.kt
@@ -1,22 +1,22 @@
+
 package com.reelive
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.ReactRootView
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "Reelive"
+    override fun getMainComponentName(): String? {
+        return "Reelive" // Make sure this matches the name of your app component
+    }
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return object : ReactActivityDelegate(this, mainComponentName) {
+            override fun createRootView(): ReactRootView {
+                return ReactRootView(this@MainActivity)
+            }
+        }
+    }
 }
+
